2b56161a4dc65ad1a5d2212e26654e618f8dcd04
Harsha Nadampalli <harshanadampalli@192.168.1.103>
2022-09-28 16:42:55 +0530

    Add conversions feature

---
+++ mygolang-lco/04conversions/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	fmt.Println("Welcome to our Pizza App")
+	fmt.Println("Please rate our pizza from 1 to 5")
+
+	reader := bufio.NewReader(os.Stdin)
+	input, _ := reader.ReadString('\n')
+
+	numRating, err := strconv.ParseFloat(strings.TrimSpace(input), 64)
+
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Printf("Added 1 to rating: %v\n", numRating+1)
+	}
+}
