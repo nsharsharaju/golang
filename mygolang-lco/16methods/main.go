7f27f1cda0856b39c7bdbbee9647a4be0bf5cf4d
Harsha Nadampalli <harshanadampalli@192.168.1.103>
2022-09-29 16:32:38 +0530

    Add class on Methods

---
+++ mygolang-lco/16methods/main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Structs in golang")
+
+	harsha := User{"Harsha", "nsharsharaju@gmail.com", true, 26}
+
+	fmt.Println(harsha)
+
+	fmt.Printf("User details are %+v\n", harsha)
+
+	harsha.GetStatus()
+	harsha.NewMail()
+}
+
+type User struct {
+	Name   string
+	Email  string
+	Status bool
+	Age    int
+}
+
+func (u User) GetStatus() {
+	fmt.Println("The status of the users is", u.Status)
+}
+
+func (u User) NewMail() {
+	u.Email = "test@go.dev"
+	fmt.Println("The new email of the users is", u.Email)
+}
